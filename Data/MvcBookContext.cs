ca8fbe196abeee686f6d87762f3a0046370af67f
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-07 19:55:48 +0300

    Initial dump

---
+++ Data/MvcBookContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+
+namespace Project.Data
+{
+    public class MvcBookContext : DbContext
+    {
+        public MvcBookContext(DbContextOptions<MvcBookContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
