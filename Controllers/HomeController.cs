9f6af5fb296039a2c2d42a66057e7902e3e765ed
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-22 00:53:50 +0300

    Changed spaces to tabs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,39 +4,45 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Project.Data;
 using Project.Models;
 
 namespace Project.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+			private readonly MvcBookContext _context;
+				private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+
+				public HomeController(MvcBookContext context,ILogger<HomeController> logger)
+				{
+            _context = context;
+						_logger=logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+				public async Task<IActionResult> Index()
+				{
+						var mvcBookContext = _context.Books.Include(b => b.Genre);
+						return View(await mvcBookContext.ToListAsync());
+				}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+				public IActionResult Privacy()
+				{
+						return View();
+				}
 
-        public IActionResult About()
-        {
-            return View();
-        }
+				public IActionResult About()
+				{
+						return View();
+				}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+				[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+				public IActionResult Error()
+				{
+						return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+				}
+	}
 }
