a008139d0c91afb4ae763d6db43ea7f721c38aee
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-21 16:57:57 +0300

    Book.cs: Removed unnecessary usings

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Project.Data;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Project.Models
 {
