60706233065d9e43848acaabd9a25337ff9feef0
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-27 08:45:33 +0300

    MvcBookController.cs: Added foreign key to Book

--- Data/MvcBookContext.cs
+++ Data/MvcBookContext.cs
@@ -4,36 +4,37 @@ using Project.Models;
 
 namespace Project.Data
 {
-    public class MvcBookContext : DbContext
-    {
-        public MvcBookContext(DbContextOptions<MvcBookContext> options):base(options)
-        {
+	public class MvcBookContext : DbContext
+	{
+		public MvcBookContext(DbContextOptions<MvcBookContext> options):base(options)
+		{
 
-        }
+		}
 
-        public DbSet<Author> Authors { get; set; }
-        public DbSet<Book> Books { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<Stockpile> Stockpiles { get; set; }
-        public DbSet<Written_By> BookAuthors { get; set; }
+		public DbSet<Author> Authors { get; set; }
+		public DbSet<Book> Books { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Stockpile> Stockpiles { get; set; }
+		public DbSet<Written_By> BookAuthors { get; set; }
 		public DbSet<Cart> Carts { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Author>().ToTable("Author");
-            modelBuilder.Entity<Book>().ToTable("Book");
-            modelBuilder.Entity<Customer>().ToTable("Customer");
-            modelBuilder.Entity<Genre>().ToTable("Genre");
-            modelBuilder.Entity<Order>().ToTable("Orders");
-            modelBuilder.Entity<Stockpile>().ToTable("Stockpile");
-            modelBuilder.Entity<Written_By>().ToTable("Written_By");
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Author>().ToTable("Author");
+			modelBuilder.Entity<Book>().ToTable("Book");
+			modelBuilder.Entity<Customer>().ToTable("Customer");
+			modelBuilder.Entity<Genre>().ToTable("Genre");
+			modelBuilder.Entity<Order>().ToTable("Orders");
+			modelBuilder.Entity<Stockpile>().ToTable("Stockpile");
+			modelBuilder.Entity<Written_By>().ToTable("Written_By");
 			modelBuilder.Entity<Cart>().ToTable("Cart");
 
-            modelBuilder.Entity<Book>().Property(b => b.Price).HasColumnType("float");
+			modelBuilder.Entity<Book>().Property(b => b.Price).HasColumnType("float");
+			modelBuilder.Entity<Book>().HasOne(c => c.Genre).WithMany(c => c.Books).IsRequired();
 
-            modelBuilder.Entity<Written_By>().HasKey(c => new { c.ISBN, c.AuthorID });
+			modelBuilder.Entity<Written_By>().HasKey(c => new { c.ISBN, c.AuthorID });
 			modelBuilder.Entity<Written_By>().HasOne(c => c.Book).WithMany(c => c.BookAuthors).HasForeignKey(bc => bc.ISBN);
 			modelBuilder.Entity<Written_By>().HasOne(c => c.Author).WithMany(c => c.Written_Books).HasForeignKey(bc => bc.AuthorID);
 
@@ -44,6 +45,6 @@ namespace Project.Data
 			modelBuilder.Entity<Order>().HasOne(c => c.Book).WithMany(c => c.Orders).HasForeignKey(c => c.BookId);
 			modelBuilder.Entity<Order>().HasOne(c => c.Customer).WithMany(c => c.Orders).HasForeignKey(c => c.CustomerId);	
 
-        }
-    }
+		}
+	}
 }
