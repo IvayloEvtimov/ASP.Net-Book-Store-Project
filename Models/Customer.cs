642b9142dfa71104281cfef5382d7b834e92b1fd
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-28 13:56:25 +0300

    Customer.cs: Added password field

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Project.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
 
         [Display(Name="Telephone Number")]
         public string TelephoneNumber { get; set; }
