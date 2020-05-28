ca1fecb7f571a65737484637ae91952a6f8aae4a
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-28 14:06:04 +0300

    Written_By.cs: AuthorID is not required

--- Models/Written By.cs
+++ Models/Written By.cs
@@ -3,7 +3,7 @@
 	public class Written_By
     {
         public long ISBN { get; set; }
-        public int AuthorID { get; set; }
+        public int? AuthorID { get; set; }
 
         public Book Book { get; set; }
         public Author Author { get; set; }
