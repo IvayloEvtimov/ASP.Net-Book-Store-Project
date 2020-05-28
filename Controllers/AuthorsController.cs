b9f64bdaecfd227fd512aef9ae12cb4b85f1be79
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-28 14:05:42 +0300

    AuthorsController.cs: Index return all authors

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -23,7 +23,9 @@ namespace Project.Controllers
 		// GET: Authors
 		public async Task<IActionResult> Index()
 		{
-			return View(await _context.Authors.ToListAsync());
+			var authors = await _context.Authors.Include(model => model.Written_Books).ThenInclude(model => model.Book)
+			.AsNoTracking().OrderBy(model => model.Name).ToListAsync();
+			return View(authors);
 		}
 
 		// GET: Authors/Details/5
@@ -34,7 +36,7 @@ namespace Project.Controllers
 				return NotFound();
 			}
 
-			var author = await _context.Authors
+			var author = await _context.Authors.Include(model => model.Written_Books).ThenInclude(model => model.Book)
 				.FirstOrDefaultAsync(m => m.ID == id);
 			if (author == null)
 			{
@@ -47,6 +49,9 @@ namespace Project.Controllers
 		// GET: Authors/Create
 		public IActionResult Create()
 		{
+			var author = new Author();
+			author.Written_Books = new List<Written_By>();
+			PopulateWrittenBookData(author);
 			return View();
 		}
 
@@ -83,11 +88,16 @@ namespace Project.Controllers
 				return NotFound();
 			}
 
-			var author = await _context.Authors.FindAsync(id);
+			var author = await _context.Authors.Include(model => model.Written_Books).ThenInclude(model => model.Book)
+			.FirstOrDefaultAsync(model => model.ID == id);
+
 			if (author == null)
 			{
 				return NotFound();
 			}
+
+			//ViewBag.Books = (from model in _context.Books orderby model.Title select model).ToList();
+			PopulateWrittenBookData(author);
 			return View(author);
 		}
 
@@ -96,34 +106,68 @@ namespace Project.Controllers
 		// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("ID,Name")] Author author)
+		public async Task<IActionResult> Edit(int? id, string[] selectedBooks)
 		{
-			if (id != author.ID)
+			if (id == null)
 			{
 				return NotFound();
 			}
 
-			if (ModelState.IsValid)
+			var authorToUpdate = await _context.Authors.Include(model => model.Written_Books).ThenInclude(model => model.Book)
+				.FirstOrDefaultAsync(model => model.ID == id);
+
+			if (await TryUpdateModelAsync<Author>(authorToUpdate, "", model => model.Name))
 			{
+				UpdateAuthorBooks(selectedBooks, authorToUpdate);
 				try
 				{
-					_context.Update(author);
 					await _context.SaveChangesAsync();
 				}
-				catch (DbUpdateConcurrencyException)
+				catch (DbUpdateException ex )
 				{
-					if (!AuthorExists(author.ID))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
+					//Log the error (uncomment ex variable name and write a log.)
+					ModelState.AddModelError("", "Unable to save changes. " +
+						"Try again, and if the problem persists, " +
+						"see your system administrator.");
 				}
 				return RedirectToAction(nameof(Index));
 			}
-			return View(author);
+			UpdateAuthorBooks(selectedBooks, authorToUpdate);
+			PopulateWrittenBookData(authorToUpdate);
+			return View(authorToUpdate);
+		}
+
+		private void UpdateAuthorBooks(string[] selectedBooks, Author authorToUpdate)
+		{
+			if (selectedBooks is null)
+			{
+				authorToUpdate.Written_Books = new List<Written_By>();
+				return;
+			}
+
+			var selectedBooksHS = new HashSet<string>(selectedBooks);
+			var writtenBooks = new HashSet<long>(authorToUpdate.Written_Books.Select(model => model.ISBN));
+
+			foreach(var book in _context.Books)
+			{
+				if(selectedBooksHS.Contains(book.ISBN.ToString()))
+				{
+					if (!writtenBooks.Contains(book.ISBN))
+					{
+						authorToUpdate.Written_Books.Add(new Written_By { ISBN = book.ISBN, AuthorID = authorToUpdate.ID });
+					}
+				}
+				else
+				{
+					if (writtenBooks.Contains(book.ISBN))
+					{
+						Written_By bookToRemove = authorToUpdate.Written_Books.FirstOrDefault(model => model.ISBN == book.ISBN);
+						_context.Remove(bookToRemove);
+					}
+				}
+			}
+
+
 		}
 
 		// GET: Authors/Delete/5
@@ -149,7 +193,10 @@ namespace Project.Controllers
 		[ValidateAntiForgeryToken]
 		public async Task<IActionResult> DeleteConfirmed(int id)
 		{
-			var author = await _context.Authors.FindAsync(id);
+			Author author = await _context.Authors.Include(model => model.Written_Books).SingleAsync(model => model.ID == id);
+			var writtenBooks = await _context.BookAuthors.Where(model => model.AuthorID == id).ToListAsync();
+			writtenBooks.ForEach(model => model.AuthorID = null);
+
 			_context.Authors.Remove(author);
 			await _context.SaveChangesAsync();
 			return RedirectToAction(nameof(Index));
@@ -158,6 +205,24 @@ namespace Project.Controllers
 		private bool AuthorExists(int id)
 		{
 			return _context.Authors.Any(e => e.ID == id);
+		}
+
+		private void PopulateWrittenBookData(Author author)
+		{
+			var allBooks = _context.Books;
+			var WrittenBooks = new HashSet<long>(author.Written_Books.Select(model => model.ISBN));
+			var viewModel = new List<WrittenBooksData>();
+			foreach(var book in allBooks)
+			{
+				viewModel.Add(new WrittenBooksData
+				{
+					ISBN = book.ISBN,
+					Title = book.Title,
+					IsWritten = WrittenBooks.Contains(book.ISBN)
+
+				});
+			}
+			ViewData["Books"] = viewModel;
 		}
 	}
 }
