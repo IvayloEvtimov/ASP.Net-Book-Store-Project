3cda8a1d5f262dc44386edf206ba87e9577939b0
Ivaylo Evtimov <57246991+IvayloEvtimov@users.noreply.github.com>
2020-05-28 14:03:59 +0300

    WrittenBooksData.cs: Helper class for checkbox

---
+++ Controllers/WrittenBooksData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class WrittenBooksData
+    {
+        public long ISBN { get; set; }
+        public string Title { get; set; }
+        public bool IsWritten { get; set; }
+    }
+}
